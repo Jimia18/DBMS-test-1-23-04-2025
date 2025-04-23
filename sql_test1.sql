bba364e18b5c3da5da2c35685e3bb9820edd2c14
Jimia18 <jimiaali5@gmail.com>
2025-04-23 13:45:03 +0300

    Add exported student finance SQL file

---
+++ sql_test1.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sql_test1
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `finance`
+--
+
+DROP TABLE IF EXISTS `finance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `finance` (
+  `StudentID` int DEFAULT NULL,
+  `Tuition_Fee` varchar(10) DEFAULT NULL,
+  `Administration_Fee` varchar(10) DEFAULT NULL,
+  `Exams_Fee` varchar(10) DEFAULT NULL,
+  `Medical_Fee` varchar(10) DEFAULT NULL,
+  KEY `StudentID` (`StudentID`),
+  CONSTRAINT `finance_ibfk_1` FOREIGN KEY (`StudentID`) REFERENCES `student` (`StudentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `finance`
+--
+
+LOCK TABLES `finance` WRITE;
+/*!40000 ALTER TABLE `finance` DISABLE KEYS */;
+INSERT INTO `finance` VALUES (235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000'),(235,'30000','5000','3000','4000'),(26,'40000','5000','2000','4000'),(1,'34000','5000','3000','4000'),(2,'23000','5000','3000','3000'),(3,'20000','5000','3000','4000'),(29,'35000','5000','2000','4000'),(127,'28000','5000','3000','4000');
+/*!40000 ALTER TABLE `finance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `StudentID` int NOT NULL,
+  `StudentName` varchar(50) DEFAULT NULL,
+  `BirthDate` varchar(100) DEFAULT NULL,
+  `AcademicYear` varchar(10) DEFAULT NULL,
+  `MobileNo` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`StudentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'Nelson','15/6/1989','Y2S1','0704231345'),(2,'Neema','29/11/2008','Y2S1','0733876987'),(3,'Caroline','18/10/2002','Y1S1','07732568448'),(26,'Nelson','22/7/2002','Y1S1','0720458674'),(29,'Judith','17/6/2003','Y4S2','0711234567'),(127,'Grace','14/1/2000','Y1S2','0733765987'),(128,'Dan','20/4/2001','Y1S2','0723456123'),(235,'Jane','12/1/1987','Y1S2','0722245321');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-23 13:20:03
